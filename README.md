140be54f43989f5b099c67d4fbcfa7b5a36d6e24
Ryan Sheehy <ryansheehy0@gmail.com>
2023-11-15 20:32:41 -0800

    Adds beginnings of server code.

--- README.md
+++ README.md
@@ -4,6 +4,10 @@ A simple social media app for just posting text.
 ## Minimum Viable Product 1(MVP1)
 ![mvp1](./black_and_white_mvp1.jpeg)
 
+- Endless scrolling
+  - Preload # of pages(10)
+  - Once reaches bottom then loading animation and loads another page(10 posts)
+
 ## Finished Product
 ![finished1](./black_and_white_end_product1.jpeg)
 ![finished2](./black_and_white_end_product2.jpeg)
---
+++ server/server.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const { ApolloServer } = require('@apollo/server')
+const { expressMiddleware } = require('@apollo/server/express4')
+const path = require("path")
+const { authMiddleware } = require("./utils/auth")
+
+// Import the two parts of a GraphQL schema
+const {typeDefs, resolvers} = require("./schemas")
+// Create the apollo server
+const server = new ApolloServer({typeDefs, resolvers})
+
+const db = require('./connection')
+
+const PORT = process.env.PORT || 3001
+const app = express()
+
+// Create a new instance of an Apollo server with the GraphQL schema
+async function startApolloServer(){
+  await server.start()
+
+  app.use(express.urlencoded({ extended: true }))
+  app.use(express.json())
+
+  // If production server(with Heroku) then use the built react app which is in the dist folder
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/dist')))
+
+    app.get('*', (req, res) => { // Any routes are redirected to the index.html. This is because react is a Single Page Application and doesn't need different html files.
+      res.sendFile(path.join(__dirname, '../client/dist/index.html'))
+    })
+  }
+
+  // Provides a GUI to for Graph QL for your client
+  // app.use('/graphql', expressMiddleware(server)) // Used without JWTs
+  app.use('/graphql', expressMiddleware(server, {
+    context: authMiddleware // Sets the context argument for GraphQL resolvers
+  }))
+
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log("Server is running")
+    })
+  })
+}
+
+startApolloServer()
